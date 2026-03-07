5750b946107a67563259042196ebd2abe5f37847
hongs0312 <hongseongho300@gmail.com>
2026-03-07 14:12:13 +0900

    동기화

--- c_c++/unclassified/liberal.cpp
+++ c_c++/unclassified/liberal.cpp
@@ -5,13 +5,21 @@
 
 using namespace std;
 
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
 int main() {
     #ifdef SH_LOCAL
     freopen("c_c++/0_IO_files/in.txt", "r", stdin);
     freopen("c_c++/0_IO_files/out.txt", "w", stdout);
     #endif
-
-    cout << "hello world!";
     
+    fast_io();
+
+    cout << "hello world!" << endl;
+
     return 0;
 }
