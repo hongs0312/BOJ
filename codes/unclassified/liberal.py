6c767444fee45e330474e5aab732c4b80742f3d1
hongs0312 <hongseongho300@gmail.com>
2026-03-28 12:20:40 +0900

    동기화

---
+++ codes/unclassified/liberal.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    f = [0] * 41
+
+    f[1] = f[2] = 1
+    for i in range(3, n+1):
+        f[i] = f[i-1] + f[i-2]
+
+    return f[n]
+
+n = int(input())
+
+print(fibonacci(n), n-2)
