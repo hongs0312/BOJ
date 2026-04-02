6379e1c3345cffad847c9e445e16a83714b99330
hongs0312 <hongseongho300@gmail.com>
2026-04-02 19:44:32 +0900

    동기화

--- codes/unclassified/liberal.cpp
+++ codes/unclassified/liberal.cpp
@@ -19,7 +19,20 @@ int main() {
     
     fast_io();
 
-    cout << "hello world!" << endl;
+    int n;
+    cin >> n;
+
+    if(n == 1)
+        return -1;
+        // cout << 42;
+    else if (n == 2) // 4?
+        cout << 5;
+    else if (n == 3) // 3
+        cout << 4;
+    else if (n == 4) // 2
+        cout << 4;
+    else if (n == 5) // 1
+        cout << 64;
 
     return 0;
 }
---
+++ codes/unclassified/rick.py
@@ -0,0 +1,11 @@
+no = ["Give you up",
+        "Let you down",
+        "Run around and desert you",
+        "Make you cry", 
+        "Say goodbye",  
+        "Tell a lie and hurt you"]
+
+if str(input()) in no:
+    print("NO")
+else:
+    print("YES")
